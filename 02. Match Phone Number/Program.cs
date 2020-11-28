8c9dd1702e467d928d6c03df2cef5b12108f01c7
IvanHrIvanov <vinko96@abv.bg>
2020-11-28 10:35:28 +0200

    Add

---
+++ 02. Match Phone Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string regexOne = @"\+([359]+)([-| ])2(\2)(\d{3})(\2)(\d{4})\b";
+
+            string digits = Console.ReadLine();
+
+            MatchCollection phones = Regex.Matches(digits, regexOne);
+
+            List<string> output = new List<string>();
+
+            foreach (Match item in phones)
+            {
+                output.Add(item.Value);
+            }
+
+            Console.Write(string.Join(", ", output));
+        }
+    }
+}
