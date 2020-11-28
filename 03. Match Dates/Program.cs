edfceeef32d3c703f447647dba4185ba6d1e4795
IvanHrIvanov <vinko96@abv.bg>
2020-11-28 13:41:34 +0200

    Add

---
+++ 03. Match Dates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>(?:\d{2}))([\-\.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            string dates = Console.ReadLine();
+
+            MatchCollection date = Regex.Matches(dates, pattern);
+
+            foreach (Match item in date)
+            {
+                Console.WriteLine($"Day: {item.Groups["day"].Value}, Month: {item.Groups["month"].Value}, Year: {item.Groups["year"].Value}");
+            }
+        }
+    }
+}
